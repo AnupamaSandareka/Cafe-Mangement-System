6f8571a3902288cd452884b9ea44793dd2bc7e87
AnupamaSandareka <gmanupama99@gmail.com>
2023-09-16 13:13:13 +0530

    get,update & delete

--- src/main/java/com/cafe/inn/cafemanagementsystem/dao/ProductDao.java
+++ src/main/java/com/cafe/inn/cafemanagementsystem/dao/ProductDao.java
@@ -3,9 +3,20 @@ package com.cafe.inn.cafemanagementsystem.dao;
 import com.cafe.inn.cafemanagementsystem.POJO.Product;
 import com.cafe.inn.cafemanagementsystem.wrapper.ProductWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ProductDao extends JpaRepository<Product,Integer> {
     List<ProductWrapper> getAllProduct();
+
+    @Modifying
+    @Transactional
+    Integer updateStatus(@Param("status") String status, @Param("id")int id);
+
+    List<ProductWrapper> getProductByCategory(@Param("id") Integer id);
+
+    ProductWrapper getProductById(@Param("id")Integer id);
 }
--- src/main/java/com/cafe/inn/cafemanagementsystem/rest/ProductRest.java
+++ src/main/java/com/cafe/inn/cafemanagementsystem/rest/ProductRest.java
@@ -2,10 +2,8 @@ package com.cafe.inn.cafemanagementsystem.rest;
 
 import com.cafe.inn.cafemanagementsystem.wrapper.ProductWrapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -21,4 +19,16 @@ public interface ProductRest {
 
     @PostMapping(path = "/updateProduct")
     ResponseEntity<String> updateProduct(@RequestBody(required = true) Map<String,String> requestMap);
+
+    @PostMapping(path = "/deleteById/{id}")
+    ResponseEntity<String> deleteProductById(@PathVariable Integer id);
+
+    @PostMapping(path = "/updateStatus")
+    ResponseEntity<String> updateStatus(@RequestBody(required = true) Map<String,String> requestMap);
+
+    @GetMapping(path = "/getByCategory/{id}")
+    ResponseEntity<List<ProductWrapper>> getByCategory(@PathVariable Integer id);
+
+    @GetMapping(path = "/getProductById/{id}")
+    ResponseEntity<ProductWrapper> getProductById(@PathVariable Integer id);
 }
